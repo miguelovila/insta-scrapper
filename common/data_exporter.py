de0e1a5b34f51c32aeda1e9364fc32141800e41a
Miguel Vila <miguel.vila.dev@gmail.com>
2020-10-06 13:49:00 +0100

    Added options for data exporting.

--- common/data_exporter.py
+++ common/data_exporter.py
@@ -4,8 +4,26 @@ import selected_post.data as SelectedPostData
 import selected_user.data as SelectedUserData
 import loggedin_user.data as LoggedUserData
 
-def export_logged_user_data_text(basicd_data = True, following_list = True, followers_list = True, posts_list = True):
-    pass
+def export_logged_user_data_text(basic_data = True, following_list = True, followers_list = True, posts_list = True):
+    if basic_data == True:
+        #something
+        pass
+
+    if following_list == True:
+        #something
+        pass
+
+    if followers_list == True:
+        #something
+        pass
+
+    if posts_list == True:
+        #something
+        pass
+
+    if (basic_data == False) and (following_list == False) and (followers_list == False) and (posts_list == False):
+        #something
+        pass
 
 def export_selected_user_data_text(basicd_data = True, following_list = True, followers_list = True, posts_list = True):
     pass
